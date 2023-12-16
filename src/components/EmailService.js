db875655c733c1119186bd8197ee3df9081d71fc
Jeff Speights <speights.jeff@gmail.com>
2023-12-16 09:46:35 -0500

    debug build

--- src/components/EmailService.js
+++ src/components/EmailService.js
@@ -1,5 +1,5 @@
 // import React from "react";
-import { Amplify } from "aws-amplify";
+// import { Amplify } from "aws-amplify";
 import { createContactUs } from "graphql/mutations";
 import { generateClient } from "aws-amplify/api";
 
