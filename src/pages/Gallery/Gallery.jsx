d40bb5888e3c1616008bf0cabdcdfb331703e171
Jeff Speights <speights.jeff@gmail.com>
2023-12-17 16:12:37 -0500

    Add Gallery spinner

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import RingLoader from "react-spinners/RingLoader";
+
+import "../../App.css";
 
 const Gallery = () => {
+  const color = "#0000FF";
+  const [loading, setLoading] = useState(true);
+
   return (
-    <div className="iframe-container">
-      <iframe
-        src="https://www.pictorem.com/gallery/Reggie.S"
-        allowfullscreen
-        width={"100%"}
-        height={"100%"}
-        sandbox="allow-same-origin allow-scripts allow-popups allow-forms allow-top-navigation"
-      ></iframe>
-    </div>
+    <>
+      <div className={`${loading ? "spinner" : "hidden"}`}>
+        <RingLoader
+          color={color}
+          size={150}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+        <h2>One Moment Please...</h2>
+      </div>
+      <div className={`${loading ? "hidden" : "iframe-container"}`}>
+        <iframe
+          src="https://www.pictorem.com/gallery/Reggie.S"
+          allowfullscreen
+          width={"100%"}
+          height={"100%"}
+          onLoad={() => setLoading(false)}
+          sandbox="allow-same-origin allow-scripts allow-popups allow-forms allow-top-navigation"
+        ></iframe>
+      </div>
+    </>
   );
 };
 
