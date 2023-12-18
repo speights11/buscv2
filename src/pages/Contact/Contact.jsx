e981c6966fa78b63f2a23c19678149502ac52c11
Jeff Speights <speights.jeff@gmail.com>
2023-12-18 13:50:15 -0500

    Add contact disclaimer

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -46,85 +46,97 @@ const Contact = () => {
   };
 
   return (
-    <div className="contact-form">
-      <h2>Contact Us</h2>
-      <div className={`${complete ? "hidden" : "thank-you"}`}>
-        <form>
-          <div className="form-row">
+    <>
+      <div className="billing">
+        <h2>ATTENTION</h2>
+        <h3>
+          For all questions pertaining to order status or billing, please
+          contact:
+        </h3>
+        <p>Tel: 1-800-650-5301</p>
+        <p>Fax: 514-504-2061</p>
+        <p>billing@pictorem.com</p>
+      </div>
+      <div className="contact-form">
+        <h1>Contact Us</h1>
+        <div className={`${complete ? "hidden" : "thank-you"}`}>
+          <form>
+            <div className="form-row">
+              <FormControl>
+                <TextField
+                  error={errorState & nameErr}
+                  helperText="Please enter name."
+                  id="nameFld"
+                  sx={{ m: 1, minWidth: "20vw", background: "#ffffff" }}
+                  label="Name"
+                  variant="filled"
+                  onChange={(e) => setName(e.target.value)}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <SupervisedUserCircleIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </FormControl>
+
+              <FormControl>
+                <TextField
+                  error={errorState & emailErr}
+                  helperText="Please enter valid email."
+                  id="emailFld"
+                  sx={{ m: 1, minWidth: "20vw", background: "#ffffff" }}
+                  label="Email address"
+                  variant="filled"
+                  onChange={(e) => setEmail(e.target.value)}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <EmailIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </FormControl>
+            </div>
+
             <FormControl>
               <TextField
-                error={errorState & nameErr}
-                helperText="Please enter name."
-                id="nameFld"
-                sx={{ m: 1, minWidth: "20vw", background: "#ffffff" }}
-                label="Name"
+                error={errorState & messageErr}
+                helperText="Please enter comments."
+                id="messageFld"
+                sx={{ m: 1, minWidth: "40vw", background: "#ffffff" }}
+                multiline
+                minRows={3}
+                label="Message"
                 variant="filled"
-                onChange={(e) => setName(e.target.value)}
+                onChange={(e) => setMessage(e.target.value)}
                 InputProps={{
                   startAdornment: (
                     <InputAdornment position="start">
-                      <SupervisedUserCircleIcon />
+                      <NoteIcon />
                     </InputAdornment>
                   ),
                 }}
               />
             </FormControl>
-
-            <FormControl>
-              <TextField
-                error={errorState & emailErr}
-                helperText="Please enter valid email."
-                id="emailFld"
-                sx={{ m: 1, minWidth: "20vw", background: "#ffffff" }}
-                label="Email address"
-                variant="filled"
-                onChange={(e) => setEmail(e.target.value)}
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <EmailIcon />
-                    </InputAdornment>
-                  ),
-                }}
-              />
-            </FormControl>
-          </div>
-
-          <FormControl>
-            <TextField
-              error={errorState & messageErr}
-              helperText="Please enter comments."
-              id="messageFld"
-              sx={{ m: 1, minWidth: "40vw", background: "#ffffff" }}
-              multiline
-              minRows={3}
-              label="Message"
-              variant="filled"
-              onChange={(e) => setMessage(e.target.value)}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <NoteIcon />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </FormControl>
-          <div className="btn">
-            <Button
-              sx={{ width: "200px" }}
-              variant="contained"
-              onClick={handleSubmit}
-            >
-              Send
-            </Button>
-          </div>
-        </form>
+            <div className="btn">
+              <Button
+                sx={{ width: "200px" }}
+                variant="contained"
+                onClick={handleSubmit}
+              >
+                Send
+              </Button>
+            </div>
+          </form>
+        </div>
+        <div className={`${complete ? "thank-you" : "hidden"}`}>
+          Thank you for reaching out. Your message has been sent.
+        </div>
       </div>
-      <div className={`${complete ? "thank-you" : "hidden"}`}>
-        Thank you for reaching out. Your message has been sent.
-      </div>
-    </div>
+    </>
   );
 };
 
