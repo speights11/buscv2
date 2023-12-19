d299e543e8cc8edb8fd1c4887070a46b9028b959
Jeff Speights <speights.jeff@gmail.com>
2023-12-19 17:07:23 -0500

    update intro image

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,7 +10,7 @@ import SupervisedUserCircleIcon from "@mui/icons-material/SupervisedUserCircle";
 import NoteIcon from "@mui/icons-material/Note";
 import Button from "@mui/material/Button";
 import { callAwsEmailSvc } from "components/EmailService";
-import gina from "assets/images/gina.png";
+import gina from "assets/images/gina-intro2.png";
 import "App.css";
 
 const errCheck = 0x0000;
