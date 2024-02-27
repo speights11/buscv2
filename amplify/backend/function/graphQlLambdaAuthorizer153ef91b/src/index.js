a8459242948d9fa671abbcd311c89874f89a2e69
Jeff Speights <speights.jeff@gmail.com>
2024-02-26 22:42:17 -0500

    New backend added

---
+++ amplify/backend/function/graphQlLambdaAuthorizer153ef91b/src/index.js
@@ -0,0 +1,46 @@
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
+const ses = new SESClient({ region: "us-east-1" });
+
+const emailAddr = "beinguscreations@gmail.com";
+
+export const handler = async (event) => {
+  for (const record of event.Records) {
+    if (record.eventName === "INSERT") {
+      //pull off items from stream
+      const page = record.dynamodb.NewImage.page.S;
+      const client = record.dynamodb.NewImage.client.S;
+      const email = record.dynamodb.NewImage.email.S;
+      const message = record.dynamodb.NewImage.message.S;
+
+      console.log("----------------------------`-------------------------");
+      console.log("-----------------------------------------------------");
+      console.log(`Page: ${page}`);
+      console.log(`Client: ${client}`);
+      console.log(`Email: ${email}`);
+      console.log(`Message: ${message}`);
+      console.log("-----------------------------------------------------");
+      console.log("-----------------------------------------------------");
+
+      const command = new SendEmailCommand({
+        Destination: {
+          ToAddresses: [emailAddr],
+        },
+        Message: {
+          Body: {
+            Text: { Data: `Message from ${client} at ${email}:\n${message}` },
+          },
+
+          Subject: { Data: `Test Email: ${page}` },
+        },
+        Source: emailAddr,
+      });
+
+      try {
+        let response = await ses.send(command);
+        return response;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+};
