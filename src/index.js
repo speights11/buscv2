c4b1c037797ba0d211d4faf96cde900f6f212c29
Jeff Speights <speights.jeff@gmail.com>
2024-03-19 14:34:51 -0400

    remove email forms

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { Amplify } from "aws-amplify";
+// import { Amplify } from "aws-amplify";
 // import config from "./amplifyconfiguration.json";
 
 // Amplify.configure(config);
