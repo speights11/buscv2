f6d41122018c850412053708a217645d566aab62
Jeff Speights <speights.jeff@gmail.com>
2024-03-24 15:22:42 -0400

    audit fix, add pre-commt hook

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 test("renders learn react link", () => {
-  render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
 });
+
+// const linkElement = screen.getByText(/learn react/i);
+// expect(linkElement).toBeInTheDocument();
